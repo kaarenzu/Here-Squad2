e88a476b358c02db30fb245cba069a38aca40737
bdiniscia <bdiniscia@gmail.com>
2020-04-01 16:13:28 -0300

    Styles of Community section

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment}from 'react';
+import React, { Fragment } from 'react';
 import { db, firebase } from '../Firebase/ConfigFirebase';
 import '../Css/post.css'
 import App from '../App.js'
@@ -11,26 +11,26 @@ class CrearPost extends React.Component {
       post: [],
       datatime: new Date(),
       mostrarPost: [],
-      name:[],
-      estado:true
+      name: [],
+      estado: true
     }
 
     this.handlePost = this.handlePost.bind(this);
     this.publicarPost = this.publicarPost.bind(this);
-    this.signOut= this.signOut.bind(this);
+    this.signOut = this.signOut.bind(this);
 
   }
-  signOut(){
+  signOut() {
     firebase.auth().signOut()
-    .then(() => {
-     this.setState({estado:false})
-      console.log('Saliendo...');
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-  
+      .then(() => {
+        this.setState({ estado: false })
+        console.log('Saliendo...');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   handlePost(event) {
     this.setState({ post: event.target.value });
     console.log(this.state.post, 'estoy tomando el post')
@@ -44,7 +44,7 @@ class CrearPost extends React.Component {
       userID: user.uid
     })
       .then((postNew) => {
-        
+
         const collecionPost = db.collection('post');
         const collecionPostOrdenada = collecionPost.orderBy('datatime', 'desc');
         collecionPostOrdenada.get().then((element) => {
@@ -53,7 +53,7 @@ class CrearPost extends React.Component {
           this.setState({
             mostrarPost: postNew,
             name: user.displayName
-          
+
 
           })
 
@@ -67,7 +67,7 @@ class CrearPost extends React.Component {
 
   }
   componentDidMount() {
-     const user = firebase.auth().currentUser;
+    const user = firebase.auth().currentUser;
     const collecionPost = db.collection('post');
     const collecionPostOrdenada = collecionPost.orderBy('datatime', 'desc');
     collecionPostOrdenada.get().then((element) => {
@@ -80,55 +80,52 @@ class CrearPost extends React.Component {
       })
 
     })
- 
+
   }
   render() {
-    if(this.state.estado){
+    if (this.state.estado) {
       return (
         <div className="containerTotal">
-          
-          <div className="containerPost">
-          <header><h1 className="textCom">
-            Comunidad Move Calm</h1>
-            <button type="submit" className="btn" id=""
-              onClick={this.signOut}>cerrar Sesion</button>
+            <header className="headerCommunity">
+              <h1 className="headerText textCom">Comunidad Move Calm</h1>
+              <h6 className="headerText subHeader">En Move Calm no estás solo:</h6>
+              {/* <button type="submit" className="btn"
+                onClick={this.signOut}>Cerrar Sesion</button> */}
             </header>
             <div className="publicarPost">
-            
-            <input type="text" className="post" id="post" placeholder="Escribe tu comentario aquí"
-              value={this.state.post} onChange={this.handlePost} />
-            <button type="submit" className="btnPost" id=""
-              onClick={this.publicarPost}>publicar</button>
-  
+
+              <textarea type="text" className="textAreaPost" id="post" placeholder="Escribe tu comentario aquí"
+                value={this.state.post} onChange={this.handlePost} />
+              <div className="divOfSend">
+                <img src={require('../img/share.png')} alt="Send button" className="buttonSend"
+                  onClick={this.publicarPost}/>
+              </div>
+
             </div>
-         
+
             <div className="containerPublic">
               {this.state.mostrarPost.map((element, key) => {
                 return (
-                  <div key={key} className="row-md-auto">
-                    <div className="mt-4">
-                      <textarea className="textArea" value={element.name+":"+"\n"+element.mensaje}
-                      ></textarea>
-  
-                    </div>
-                  </div>
+                      <div className="postArea" key={key}>
+                        <p className="textPost textUser">{element.name + ":"}</p>
+                        <p className="textPost textMessage">{element.mensaje}</p> 
+                      </div>
                 )
               })
               }
             </div>
-          </div>
         </div>
-  
+
       )
     }
     return (
       <Fragment>
-        {this.state.estado? null : <App/>}
+        {this.state.estado ? null : <App />}
 
       </Fragment>
     )
 
-    
+
   }
 }
 export default CrearPost;
